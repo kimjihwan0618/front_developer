1108845f47011ecc33af6553483474564a348496
KimAJksldq <wlghks0106@naver.com>
2023-12-10 13:51:28 +0900

    page main color changed

--- pages/components/contact.vue
+++ pages/components/contact.vue
@@ -2,12 +2,8 @@
   <section class="sub-section contact">
     <div class="loading__wrap" ref="loadingWrap">
       <div class="loading__bg"></div>
-      <lottie-vue-player
-        :theme="options.theme"
-        :player-size="options.playerSize"
-        :player-controls="true"
-        style="width: 100%; height:400px"
-      >
+      <lottie-vue-player :theme="options.theme" :player-size="options.playerSize" :player-controls="true"
+        style="width: 100%; height:400px">
       </lottie-vue-player>
     </div>
     <div class="section-title">
@@ -26,6 +22,10 @@
               <input type="email" calss="email" name="user_email" />
             </li>
             <li>
+              <label for="phone">폰번호</label>
+              <input type="phone" calss="phone" name="user_phone" />
+            </li>
+            <li>
               <label for="text">메세지 입력</label>
               <textarea name="message"></textarea>
             </li>
@@ -33,16 +33,16 @@
         </div>
         <div class="right">
           <dl>
-            <dt>Phone :&nbsp;</dt>
-            <dd>&nbsp;010 5838 5146</dd>
+            <dt>폰번호 :&nbsp;</dt>
+            <dd>&nbsp;010-5838-5146</dd>
           </dl>
           <dl>
-            <dt>E-mail :&nbsp;</dt>
+            <dt>이메일 :&nbsp;</dt>
             <dd>&nbsp;wlghks0618@kakao.com</dd>
           </dl>
           <dl>
-            <dt>Adress :&nbsp;</dt>
-            <dd>&nbsp;화성시 반월동</dd>
+            <dt>주소 :&nbsp;</dt>
+            <dd>&nbsp;경기도 용인시 기흥구</dd>
           </dl>
         </div>
       </div>
@@ -84,30 +84,80 @@ export default {
       if (
         e.target[0].value === "" ||
         e.target[1].value === "" ||
-        e.target[2].value === ""
+        e.target[2].value === "" ||
+        e.target[3].value === ""
       ) {
         alert("공백을 채워주세요!");
       } else {
-        this.$refs.loadingWrap.classList.add("on");
-        emailjs
-          .sendForm(
-            "service_wztk4bx",
-            "template_2edfiap",
-            e.target,
-            "user_mrPFHtVDlQimcAtxWCbaK"
-          )
-          .then(
-            result => {
-              console.log("SUCCESS!", result.status, result.text);
-              alert("전송이 완료되었습니다 감사합니다^-^");
-              this.$refs.loadingWrap.classList.remove("on");
-            },
-            error => {
-              console.log("FAILED...", error);
-              alert("전송에 실패하였습니다. 다시 시도해주세요!");
-              this.$refs.loadingWrap.classList.remove("on");
-            }
-          );
+        const userId = e.target[0].value
+        const email = e.target[1].value
+        const phone = e.target[2].value
+        const message = e.target[3].value
+        const userEmail = "jhkim5@drimsys.com"
+        if (/^01([0|1|6|7|8|9]?)\d{3,4}\d{4}$/.test(phone)) {
+          const param = {
+            phone: phone,
+            userId,
+            ptId: "hodo_private",
+            pw: 'WlGhks010!@#',
+            from: email,
+            to: userEmail,
+            subject: `이직문의`,
+            title: "안녕하세요 이직 문의드립니다.",
+            body: `
+        <div style="margin: 16px 0 0 0; padding: 12px 12px 60px 12px;box-sizing: border-box;">
+          <div style="padding: 34px; box-sizing: border-box;background: #fff;border-radius: 12px;box-shadow: 0 0.75rem 2rem 0 rgba(0, 0, 0, 0.1);">
+            <h1 style="margin: 0;padding: 0; color: #414141;">'${userId}'님의 사이트로부터 문의 메일이 도착했습니다.</h1>
+            <p style="color: #999; margin: 8px 0 34px 0; padding: 0;">메일 내용은 아래와 같습니다.</p>
+            <ul style="list-style: none;margin: 0 0 38px 0;padding: 0 0 18px 0; border-bottom: 1px solid rgb(231, 231, 231);">
+              <li style="margin-bottom: 12px;">
+                <dl style="display: flex;align-items: flex-start;">
+                  <dt style="min-width: 60px;opacity: .6; white-space: nowrap;">제목&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;</dt>
+                  <dd style="margin: 0;padding: 0; font-weight: 600; opacity: .7">[이직문의] '${userId}'님으로부터 이직 문의가 들어왔습니다.</dd>
+                </dl>  
+              </li>
+              <li style="margin-bottom: 12px;">
+                <dl style="display: flex;align-items: flex-start;">
+                  <dt style="min-width: 60px;opacity: .6; white-space: nowrap;">이메일&nbsp;&nbsp;:&nbsp;&nbsp;</dt>
+                  <dd style="margin: 0;padding: 0; font-weight: 600; opacity: .7;">${email}</dd>
+                </dl>  
+              </li>
+              <li>
+                <dl style="display: flex;align-items: flex-start;">
+                  <dt style="min-width: 60px;opacity: .6; white-space: nowrap;">연락처&nbsp;&nbsp;:&nbsp;&nbsp;</dt>
+                  <dd style="margin: 0;padding: 0; font-weight: 600; opacity: .7;">${phone}</dd>
+                </dl>  
+              </li>
+            </ul>
+            <p style="margin-top:30px;">${message}</p>
+          </div>
+        </div>
+        `,
+          };
+          try {
+            fetch('https://kimjihodo.synology.me:3001/email/send', {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify(param),
+            })
+              .then((response) => response.text())
+              .then((text) => {
+                alert('메일 전송되었습니다!');
+                this.$refs.loadingWrap.classList.remove("on");
+              })
+              .catch((error) => {
+                console.log(error);
+                alert('메일 전송이 실패했습니다.');
+                this.$refs.loadingWrap.classList.remove("on");
+              });
+          } catch (error) {
+            console.log(error);
+            alert('메일 전송이 실패했습니다.');
+            this.$refs.loadingWrap.classList.remove("on");
+          }
+        } else {
+          alert("폰번호를 확인해주세요!");
+        }
       }
     }
   }
