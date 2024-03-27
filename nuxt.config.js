7c28d0f319483cd8d02ad9abc8fbb99ce5a842e4
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-03-27 21:08:25 +0900

    소스머지

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,17 @@
 export default {
   target: "static",
-  // router: {
-  //   base: "/portfolio/"
-  // },
+  router: {
+    base: "/front_developer/",
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    // title: "김지환 포트폴리오",
+    title: "김지환 포트폴리오",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { hid: "description", name: "description", content: "" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "./favicon.ico" }],
     // script : [
     //   { src : "https://maps.googleapis.com/maps/api/js?key=AIzaSyBq0QlIOuHsix2pUKYJkok0B6TMsooqsEc&callback=&language=en&callback=initMap"}
     // ]
@@ -21,9 +21,7 @@ export default {
   css: ["@/assets/css/style.css"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    "~/plugins/lottie-vue-player.client.js"
-  ],
+  plugins: ["~/plugins/lottie-vue-player.client.js"],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -39,5 +37,5 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
     // transpile: [/^vue2-google-maps($|\/)/],
-  }
+  },
 };
